ddd654fdb95b7ec305c3d347e998bb1636c3c29d
Idan Vasenko <idanvas@gmail.com>
2023-02-17 12:30:56 +0200

    change from flex to grid

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ import { CgLock as LockedIcon, CgLockUnlock as OpenIcon} from "react-icons/cg";
 function App() {
 
   return (
-    <FlexContainer>
-      <Ul>
+    <GridContainer>
         <Div>
           <h1>1</h1>
           <Button><OpenIcon size={30}/></Button>
@@ -27,38 +26,28 @@ function App() {
           <h1>5</h1>
           <Button><OpenIcon size={30}/></Button>
         </Div>
-      </Ul>
-    </FlexContainer>
+    </GridContainer>
   )
 };
 
 export default App;
 
 
-const FlexContainer = styled.div`
-  /* background-color: black; */
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-  /* align-items: stretch; */
-  height: 100vh;
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5 , 1fr);
   `;
 
 const Div = styled.div`
   background-color: green;
-  border: 2px black dashed;
   height: 100vh;
 
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
+  display: grid;
+  grid-template-rows: 1fr ,1fr;
+  justify-items: center;
   align-items: center;
-  flex: 1 1;
 `;
 
-const Ul = styled.ul`
-  display: flex;
-`;
 const Button = styled.button`
 height: 40px;
 width: 40px;
